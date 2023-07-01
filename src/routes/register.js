9a1f0f811e0c20ee2188d8308fdc0dc9f2b3db19
Anish-Udupa <udupa.anish@gmail.com>
2023-07-01 15:10:12 +0530

    finished register

--- src/routes/register.js
+++ src/routes/register.js
@@ -3,7 +3,12 @@ const User = require('../models/User.js');
 const register = (req, res) => {
     const { username, password } = req.body;
     const user = new User({ username, password });
-    
+    user.save((err) => {
+        if(err)
+            res.status(500).send({ status: 'fail' });
+        else
+            res.send({ status: 'success' });
+    })
 }
 
 module.exports = register;
