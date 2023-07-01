8802c17dc2762b2920a686db26a01fd3480ac151
Anish-Udupa <udupa.anish@gmail.com>
2023-07-01 17:09:56 +0530

    Added search functionality

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-
-const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     username: {
@@ -12,23 +9,6 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     }
-})
-
-// Hash the password before saving
-userSchema.pre("save", (next) => {
-    if(this.isNew || this.isModified('password')) {
-        const document = this;
-        bcrypt.hash(document.password, saltRounds, (err, hash) => {
-            if (err)
-                next(err);
-            else {
-                document.password = hash;
-                next();
-            }
-        })
-    }
-    else 
-        next();
 })
 
 
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 var express = require('express')
 const login = require('./routes/login')
-const register = require('./routes/register')
+const register = require('./routes/register');
+const search = require('./routes/search');
 var router = express.Router()
 
 
 // Add routes here
 router.post("/register", register);
 router.get("/login", login)
+router.get('/search', search)
 
 module.exports = router
