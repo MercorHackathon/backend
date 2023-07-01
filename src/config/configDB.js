880ff119cb14757392087bf8863f423004e1f842
Anish-Udupa <udupa.anish@gmail.com>
2023-07-01 19:15:03 +0530

    Fixed configDB

--- src/config/configDB.js
+++ src/config/configDB.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 const configDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-          useUnifiedTopology:true,
+            useUnifiedTopology: true,
             useNewUrlParser: true,
-            useCreateIndex: true
         })
         console.log("Mongoose connected!")
     }
